b22d997a3e80e5c96c3bc6e574da5dd6f802b08b
Baasim Mustafa <mustab2@mcmaster.ca>
2025-01-16 19:10:30 -0500

    added history page, fixed up showing balance

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,43 @@
-
 document.getElementById("balanceForm").addEventListener("submit", function (event) {
-    event.preventDefault()
+    event.preventDefault() 
+
     const balance = parseFloat(document.getElementById("startingBalance").value)
+
     if (!isNaN(balance)) {
         localStorage.setItem("startingBalance", balance)
+
         alert(`Your starting balance is $${balance.toFixed(2)}!`)
 
         document.querySelector(".starting-balance").style.display = "none"
+
+        document.querySelector(".expense-tracker").style.display = "block"
     }
 })
 
 document.getElementById("expenseForm").addEventListener("submit", function (event) {
-    event.preventDefault()
+    event.preventDefault() 
+
     const expenseName = document.getElementById("expenseName").value
     const expenseAmount = parseFloat(document.getElementById("expenseAmount").value)
 
-    if (expenseName && !isNaN(expenseAmount)) {
+    let balance = parseFloat(localStorage.getItem("startingBalance"))
+
+    if (expenseName && !isNaN(expenseAmount) && !isNaN(balance)) {
+        const newBalance = balance - expenseAmount
+
+        localStorage.setItem("startingBalance", newBalance)
+
         const tableBody = document.querySelector("#expenseTable tbody")
         const newRow = document.createElement("tr")
 
         newRow.innerHTML = `
             <td>${expenseName}</td>
             <td>$${expenseAmount.toFixed(2)}</td>
+            <td>$${newBalance.toFixed(2)}</td>
         `
 
         tableBody.appendChild(newRow)
 
-        const balance = parseFloat(localStorage.getItem("startingBalance"))
-        const newBalance = balance - expenseAmount
-        localStorage.setItem("startingBalance", newBalance)
         alert(`Remaining Balance: $${newBalance.toFixed(2)}`)
     }
 })
